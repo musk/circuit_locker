b26502c887e026d93daf27c994d759259ea8870a
Stefan Langer <mailtolanger@googlemail.com>
2015-04-06 14:30:48 +0200

    Fixed license information and added docs

--- TestKeypad/TestKeypad.ino
+++ TestKeypad/TestKeypad.ino
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see http://www.gnu.org/licenses
  *
  * This program uses parts of the arduino core libraries published by Arduino LLC
  * which are released under the LGPL v2.1 or later.
@@ -23,11 +23,12 @@
  * Creative Commons Attribution-ShareAlike 4.0 International License
  * http://creativecommons.org/licenses/by-sa/4.0
  */
+// define the pins used for the rows
 #define ROW1 15
 #define ROW2 14
 #define ROW3 13
 #define ROW4 12
-
+// define the pins used to read the state of the columns
 #define COL1 11
 #define COL2 10
 #define COL3 9
@@ -35,15 +36,23 @@
 
 #define uint unsigned int
 
+// helper constructs to easily manage the rows and columns
 const uint ROWS[] = {ROW1, ROW2, ROW3, ROW4};
 const uint COLS[]  = {COL1, COL2, COL3, COL4};
+// matrix representation of the keypad
+// the coordinate of each key is (row, column)
 const char LETTERS[][4] = {{'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
 
-
+/**
+ * Returns the currently pressed key.
+ *
+ * @return the pressed key or -1 if 
+ *         no key press was registered.
+ */
 char getPressedKey() {
   for (int ri = 0; ri < 4; ri++) {
     digitalWrite(ROWS[ri], HIGH);
@@ -59,18 +68,23 @@ char getPressedKey() {
 
 
 void setup() {
-  // setup pins
+  // setup row pins for output and col pins for input 
+  // and set each one to LOW.
   for (int i = 0; i < 4; i++) {
     pinMode(ROWS[i], OUTPUT);
     pinMode(COLS[i], INPUT);
     digitalWrite(ROWS[i], LOW);
     digitalWrite(COLS[i], LOW);
   }
+  // start serial montior for debugging purposes
   Serial.begin(9600);
   Serial.println("Initialized and ready to go!");
 }
 
 void loop() {
+  // read the current key press and if
+  // available print it to the serial
+  // monitor
   char key = getPressedKey();
   if (key != -1) {
     Serial.print("Got key press: ");
--- TestLCD/TestLCD.ino
+++ TestLCD/TestLCD.ino
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see http://www.gnu.org/licenses.
  *
  * This program uses parts of the arduino core libraries published by Arduino LLC
  * which are released under the LGPL v2.1 or later.
